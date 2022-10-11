6dbd14a69c102ee71eab59f6a5abb2c43e2cf060
Jake <jmorgan3142001@gmail.com>
2022-10-10 21:17:16 -0500

    Comment Added to Waypoint
    Added comment to test that GitHub is properly running on my end

--- CapstoneProject/Assets/Scripts/Waypoints.cs
+++ CapstoneProject/Assets/Scripts/Waypoints.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class Waypoints : MonoBehaviour
 {
+     //Array of points to which enemies will travel before reaching their target
+
     public Transform[] waypoints;
 
 }
